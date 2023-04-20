9139fabc53ebde95f5d74eaa657f53247280fb61
Pablenko <pawel.przybylski91@gmail.com>
2023-04-20 22:49:10 +0200

    fmt

--- calendar/src/main.rs
+++ calendar/src/main.rs
@@ -1,15 +1,23 @@
 use chrono::{Datelike, Weekday};
 
-
 fn generate_calendar(date: &chrono::NaiveDate) {
     println!("\n{}\n", date.format("%B %Y").to_string());
     println!("Mon Tue Wed Thu Fri Sat Sun\n");
 
     let mut next_line = String::new();
     let days_from_monday = date.weekday().num_days_from_monday() as usize;
-    next_line.push_str(std::iter::repeat("    ").take(days_from_monday).collect::<String>().as_str());
+    next_line.push_str(
+        std::iter::repeat("    ")
+            .take(days_from_monday)
+            .collect::<String>()
+            .as_str(),
+    );
 
-    for (day_idx, day) in date.iter_days().take_while(|d| d.month() == date.month()).enumerate() {
+    for (day_idx, day) in date
+        .iter_days()
+        .take_while(|d| d.month() == date.month())
+        .enumerate()
+    {
         next_line.push_str(format!("{:<4}", day_idx + 1).as_str());
         if day.weekday() == Weekday::Sun {
             println!("{}", next_line);
@@ -27,8 +35,7 @@ fn main() {
     let date = chrono::NaiveDate::parse_from_str(&input, "%d-%m-%Y");
     if let Ok(date) = date {
         generate_calendar(&date);
-    }
-    else {
+    } else {
         println!("Invalid date");
     }
 }
