cdb67afafca59944c5ad2b0099eb753caba4cf30
Pablenko <pawel.przybylski91@gmail.com>
2023-05-01 22:08:06 +0200

    aggregation for 1 car working

--- ev_flex/src/main.rs
+++ ev_flex/src/main.rs
@@ -60,11 +60,17 @@ fn create_flex_series(demand: &EnergyDemand) -> Option<Vec<AggregationDT>> {
     let mut aggregation_series = vec![];
     let one_percent_energy = demand.capacity / 100;
     let energy_demand = (demand.target_soc - demand.current_soc) * one_percent_energy;
-    let time_to_charge_minutes = (energy_demand / demand.max_charging_power * 60) as i64;
+    let critical_demand = match demand.min_soc > demand.current_soc {
+        true => (demand.min_soc - demand.current_soc) * one_percent_energy,
+        false => 0,
+    };
+    let time_to_charge_max_soe = (energy_demand * 60 / demand.max_charging_power) as i64;
+    let time_to_charge_critical_soe = (critical_demand * 60 / demand.max_charging_power) as i64;
     let one_minute_energy_state_change = demand.max_charging_power / 60;
 
-    let asap_charge_time_end = demand.start + Duration::minutes(time_to_charge_minutes);
-    let alap_charge_time_end = demand.end - Duration::minutes(time_to_charge_minutes);
+    let asap_charge_time_end = demand.start + Duration::minutes(time_to_charge_max_soe);
+    let alap_charge_time_start = demand.end - Duration::minutes(time_to_charge_max_soe)
+        + Duration::minutes(time_to_charge_critical_soe);
 
     if asap_charge_time_end > demand.end {
         println!("UNFEASIBLE DEMAND");
@@ -95,7 +101,7 @@ fn create_flex_series(demand: &EnergyDemand) -> Option<Vec<AggregationDT>> {
     }
 
     // Fill middle area
-    for time in MinuteDateRange(asap_charge_time_end, alap_charge_time_end) {
+    for time in MinuteDateRange(asap_charge_time_end, alap_charge_time_start) {
         aggregation_series.push(AggregationDT {
             min_soe: min_soe_state,
             max_soe: max_soe_state,
@@ -105,7 +111,7 @@ fn create_flex_series(demand: &EnergyDemand) -> Option<Vec<AggregationDT>> {
     }
 
     // Fill alap line
-    for time in MinuteDateRange(alap_charge_time_end, demand.end) {
+    for time in MinuteDateRange(alap_charge_time_start, demand.end) {
         aggregation_series.push(AggregationDT {
             min_soe: min_soe_state,
             max_soe: max_soe_state,
