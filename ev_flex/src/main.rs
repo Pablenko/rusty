f1ef79f943c2f328b94c5c3f94cc54323b3cd0ac
Pablenko <pawel.przybylski91@gmail.com>
2023-05-01 18:58:47 +0200

    Series are generated (not working yet)

--- ev_flex/src/main.rs
+++ ev_flex/src/main.rs
@@ -1,5 +1,6 @@
 use actix_web::{get, post, web, web::Data, App, HttpServer, Responder};
 use chrono::prelude::{DateTime, Utc};
+use chrono::Duration;
 use serde::{Deserialize, Serialize};
 use std::sync::Mutex;
 
@@ -21,6 +22,7 @@ struct AggregationDT {
     min_soe: i32,            // minimum state of charge in Wh
     max_soe: i32,            // maximum state of charge in Wh
     max_charging_power: i32, // maximum charging power in W
+    time: DateTime<Utc>,     // time
 }
 
 #[derive(Serialize, Deserialize)]
@@ -38,6 +40,82 @@ impl Default for Aggregation {
             series: vec![],
         }
     }
+}
+
+struct MinuteDateRange(DateTime<Utc>, DateTime<Utc>);
+
+impl Iterator for MinuteDateRange {
+    type Item = DateTime<Utc>;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.0 <= self.1 {
+            let next = self.0 + Duration::minutes(1);
+            Some(std::mem::replace(&mut self.0, next))
+        } else {
+            None
+        }
+    }
+}
+
+fn create_flex_series(demand: &EnergyDemand) -> Option<Vec<AggregationDT>> {
+    let mut aggregation_series = vec![];
+    let one_percent_energy = demand.capacity / 100;
+    let energy_demand = (demand.target_soc - demand.current_soc) * one_percent_energy;
+    let time_to_charge_minutes = (energy_demand / demand.max_charging_power * 60) as i64;
+    let one_minute_energy_state_change = demand.max_charging_power / 60;
+
+    let asap_charge_time_end = demand.start + Duration::minutes(time_to_charge_minutes);
+    let alap_charge_time_end = demand.end - Duration::minutes(time_to_charge_minutes);
+
+    if asap_charge_time_end > demand.end {
+        println!("UNFEASIBLE DEMAND");
+        return None;
+    }
+
+    let mut max_soe_state = demand.current_soc * one_percent_energy;
+    let mut min_soe_state = demand.current_soc * one_percent_energy;
+
+    let min_soe = demand.min_soc * one_percent_energy;
+
+    // Fill asap line
+    for time in MinuteDateRange(demand.start, asap_charge_time_end) {
+        let aggregation_dt = AggregationDT {
+            min_soe: min_soe_state,
+            max_soe: max_soe_state,
+            max_charging_power: demand.max_charging_power,
+            time: time,
+        };
+
+        aggregation_series.push(aggregation_dt);
+
+        max_soe_state += one_minute_energy_state_change;
+
+        if min_soe_state + one_minute_energy_state_change < min_soe {
+            min_soe_state += one_minute_energy_state_change;
+        }
+    }
+
+    // Fill middle area
+    for time in MinuteDateRange(asap_charge_time_end, alap_charge_time_end) {
+        aggregation_series.push(AggregationDT {
+            min_soe: min_soe_state,
+            max_soe: max_soe_state,
+            max_charging_power: demand.max_charging_power,
+            time: time,
+        });
+    }
+
+    // Fill alap line
+    for time in MinuteDateRange(alap_charge_time_end, demand.end) {
+        aggregation_series.push(AggregationDT {
+            min_soe: min_soe_state,
+            max_soe: max_soe_state,
+            max_charging_power: demand.max_charging_power,
+            time: time,
+        });
+        min_soe_state += one_minute_energy_state_change;
+    }
+
+    Some(aggregation_series)
 }
 
 struct Demands {
@@ -72,11 +150,11 @@ async fn handle_aggregation_request(db: Data<Demands>) -> impl Responder {
     let demands = db.demands.lock().unwrap();
     let mut aggregation: Aggregation = Aggregation::default();
     for demand in demands.iter() {
-        aggregation.series.push(AggregationDT {
-            min_soe: demand.min_soc,
-            max_soe: demand.max_soc,
-            max_charging_power: demand.max_charging_power,
-        });
+        aggregation.start = std::cmp::min(aggregation.start, demand.start);
+        aggregation.end = std::cmp::max(aggregation.end, demand.end);
+        if let Some(mut series) = create_flex_series(demand) {
+            aggregation.series.append(&mut series);
+        }
     }
     serde_json::to_string(&aggregation)
 }
